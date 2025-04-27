4d76517d17fabb1fbc57b8a9341a9d9a704e8bfd
Richard Kweku Aikins <richardkns7@gmail.com>
2025-04-27 12:28:51 +0000

    Update location_service.dart

--- lib/features/Home/services/location_service.dart
+++ lib/features/Home/services/location_service.dart
@@ -1,23 +1,23 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:cloud_firestore/cloud_firestore.dart';
 
-class LocationService {
-  final FirebaseFirestore _db = FirebaseFirestore.instance;
-  final String schools = 'schools';
-  final String history = 'history';
+// class LocationService {
+//   final FirebaseFirestore _db = FirebaseFirestore.instance;
+//   final String schools = 'schools';
+//   final String history = 'history';
 
-  Future<void> addLocation(
-      String name, String address, String latitude, String longitude) async {
-    await _db.collection(schools).add({
-      'name': name,
-      'address': address,
-      'latitude': latitude,
-      'longitude': longitude,
-    });
-  }
+//   Future<void> addLocation(
+//       String name, String address, String latitude, String longitude) async {
+//     await _db.collection(schools).add({
+//       'name': name,
+//       'address': address,
+//       'latitude': latitude,
+//       'longitude': longitude,
+//     });
+//   }
 
-  // get all locations
-  Future<List<DocumentSnapshot>> getSchools() async {
-    QuerySnapshot querySnapshot = await _db.collection(schools).get();
-    return querySnapshot.docs;
-  }
-}
+//   // get all locations
+//   Future<List<DocumentSnapshot>> getSchools() async {
+//     QuerySnapshot querySnapshot = await _db.collection(schools).get();
+//     return querySnapshot.docs;
+//   }
+// }
