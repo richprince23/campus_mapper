30422beb45728c452d3bdc51587688a9209c9a37
Richard Kweku Aikins <richardkns7@gmail.com>
2025-04-27 13:53:21 +0000

    Update map_provider.dart

--- lib/features/Explore/providers/map_provider.dart
+++ lib/features/Explore/providers/map_provider.dart
@@ -1,17 +1,64 @@
-import 'dart:developer';
+import 'dart:developer' show log;
 
 import 'package:campus_mapper/features/Explore/models/location.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapProvider extends ChangeNotifier {
-  Set<Marker> _markers = {};
+  final Set<Marker> _markers = {};
 
   Future<BitmapDescriptor> getCustomIcon(String category) async {
     return await BitmapDescriptor.asset(
       const ImageConfiguration(size: Size(24, 24)), // Set size
       _getCategoryMarker(category), // Path to the asset
     );
+  }
+
+  Future<BitmapDescriptor> getCustomIconColor(String category) async {
+    try {
+      return await BitmapDescriptor.defaultMarkerWithHue(
+          _getCategoryHue(category));
+    } catch (e) {
+      log('Error getting custom icon: $e');
+      return BitmapDescriptor.defaultMarker;
+    }
+  }
+
+  double _getCategoryHue(String category) {
+    switch (category.toLowerCase()) {
+      case 'class':
+      case 'classes':
+        return BitmapDescriptor.hueAzure;
+      case 'food':
+      case 'restaurant':
+      case 'bars & pubs':
+        return BitmapDescriptor.hueOrange;
+      case 'pharmacy':
+      case 'pharmacies':
+      case 'hospital':
+        return BitmapDescriptor.hueRed;
+      case 'office':
+      case 'offices':
+        return BitmapDescriptor.hueViolet;
+      case 'atm':
+      case 'atms':
+        return BitmapDescriptor.hueCyan;
+      case 'gym':
+      case 'gyms':
+        return BitmapDescriptor.hueYellow;
+      case 'hostels':
+      case 'hostel':
+        return BitmapDescriptor.hueMagenta;
+      case 'store':
+      case 'shop':
+      case 'shopping centers':
+        return BitmapDescriptor.hueGreen;
+      case 'church':
+      case 'churches':
+        return BitmapDescriptor.hueRose;
+      default:
+        return BitmapDescriptor.hueRed;
+    }
   }
 
   String _getCategoryMarker(String category) {
@@ -31,22 +78,65 @@ class MapProvider extends ChangeNotifier {
     }
   }
 
+  // void addMarker(Location data) async {
+  //   _markers.add(
+  //     Marker(
+  //       markerId: MarkerId(
+  //         data.id.toString(),
+  //       ),
+  //       position: LatLng(
+  //         data.location['latitude'],
+  //         data.location['longitude'],
+  //       ),
+  //       infoWindow: InfoWindow(title: data.name, snippet: data.description),
+  //       draggable: false,
+  //       icon: await getCustomIcon(data.category),
+  //     ),
+  //   );
+  //   notifyListeners();
+  // }
+
   void addMarker(Location data) async {
-    _markers.add(
-      Marker(
-        markerId: MarkerId(
-          data.id.toString(),
+    try {
+      _markers.add(
+        Marker(
+          markerId: MarkerId(data.id?.toString() ?? DateTime.now().toString()),
+          position: LatLng(
+            data.location['latitude'],
+            data.location['longitude'],
+          ),
+          infoWindow: InfoWindow(
+            title: data.name,
+            snippet: data.description ?? data.category,
+            onTap: () {
+              // InfoWindow tap handler could be added here
+            },
+          ),
+          draggable: false,
+          icon: await getCustomIcon(data.category),
         ),
-        position: LatLng(
-          data.location['latitude'],
-          data.location['longitude'],
+      );
+      notifyListeners();
+    } catch (e) {
+      log('Error adding marker: $e');
+    }
+  }
+
+  void addUserLocationMarker(LatLng position) {
+    try {
+      _markers.add(
+        Marker(
+          markerId: const MarkerId('user_location'),
+          position: position,
+          infoWindow: const InfoWindow(title: 'Your Location'),
+          icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueBlue),
+          zIndex: 2, // Higher zIndex to appear above other markers
         ),
-        infoWindow: InfoWindow(title: data.name, snippet: data.description),
-        draggable: false,
-        icon: await getCustomIcon(data.category),
-      ),
-    );
-    notifyListeners();
+      );
+      notifyListeners();
+    } catch (e) {
+      print('Error adding user location marker: $e');
+    }
   }
 
   ///Get all markers
