55d9fa51ce53a048127e9f7b53e96afdf9d23845
Richard Kweku Aikins <richardkns7@gmail.com>
2025-04-27 12:28:51 +0000

    Add supabase_client.dart

---
+++ lib/core/api/supabase_client.dart
@@ -0,0 +1,27 @@
+// lib/core/services/supabase_client.dart
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+class SupabaseClient {
+  static late final SupabaseClient _instance;
+  static bool _initialized = false;
+
+  static Future<void> initialize({
+    required String supabaseUrl,
+    required String supabaseAnonKey,
+  }) async {
+    if (!_initialized) {
+      await Supabase.initialize(
+        url: supabaseUrl,
+        anonKey: supabaseAnonKey,
+      );
+      _initialized = true;
+    }
+  }
+
+  static SupabaseClient get instance {
+    if (!_initialized) {
+      throw Exception('SupabaseClient must be initialized before use');
+    }
+    return SupabaseClient.instance;
+  }
+}
