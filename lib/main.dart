d88dca81665949a1ba198bca4272bd51b001f53a
Richard Kweku Aikins <richardkns7@gmail.com>
2025-06-29 17:27:26 +0000

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:campus_mapper/core/custom_theme.dart';
 import 'package:campus_mapper/features/Explore/providers/map_provider.dart';
 import 'package:campus_mapper/features/Home/pages/main_screen.dart';
+import 'package:campus_mapper/firebase_options.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_foreground_task/flutter_foreground_task.dart';
 import 'package:flutter_keyboard_visibility/flutter_keyboard_visibility.dart';
@@ -12,17 +16,22 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   FlutterForegroundTask.initCommunicationPort(); // init foreground task
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  // use firebase emulator
+  await FirebaseAuth.instance.useAuthEmulator('localhost', 9099);
+  await FirebaseStorage.instance.useStorageEmulator('localhost', 5001);
+  FirebaseFirestore.instance.useFirestoreEmulator('localhost', 8080);
+
   // Initialize Supabase with your project URL and anon key
-  await Supabase.initialize(
-    url: "https://ldwnikdoqijibgkwebgj.supabase.co",
-    anonKey:
-        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxkd25pa2RvcWlqaWJna3dlYmdqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU3MzcwMzMsImV4cCI6MjA2MTMxMzAzM30.vH0YTnKzE3x8tFJ4odjxrLG43HU-cfWRMzSGieylbnI",
-    authOptions: FlutterAuthClientOptions(
-      autoRefreshToken: true,
-      localStorage:
-          SharedPreferencesLocalStorage(persistSessionKey: "_auth_token"),
-    ),
-  );
+  // await Supabase.initialize(
+  //   url: "https://ldwnikdoqijibgkwebgj.supabase.co",
+  //   anonKey:
+  //       "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxkd25pa2RvcWlqaWJna3dlYmdqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU3MzcwMzMsImV4cCI6MjA2MTMxMzAzM30.vH0YTnKzE3x8tFJ4odjxrLG43HU-cfWRMzSGieylbnI",
+  //   authOptions: FlutterAuthClientOptions(
+  //     autoRefreshToken: true,
+  //     localStorage:
+  //         SharedPreferencesLocalStorage(persistSessionKey: "_auth_token"),
+  //   ),
+  // );
   runApp(
     MultiProvider(
       providers: [
