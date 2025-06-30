f2764131ecb8a844919db4bd48020b8ccef61f22
Richard Kweku Aikins <richardkns7@gmail.com>
2025-06-30 01:23:37 +0000

    Update main_screen.dart

--- lib/features/Home/pages/main_screen.dart
+++ lib/features/Home/pages/main_screen.dart
@@ -1,3 +1,4 @@
+import 'package:campus_mapper/core/api/route_service.dart';
 import 'package:campus_mapper/core/components/navbar.dart';
 import 'package:campus_mapper/features/Explore/pages/expore_screen.dart';
 import 'package:campus_mapper/features/History/pages/history_screen.dart';
@@ -21,10 +22,29 @@ class _MainScreenState extends State<MainScreen> {
 
   int _currentPage = 0;
 
+  void getCurrentLocation() {
+    RouteService.getCurrentLocation().then((value) {
+      if (value != null) {
+        // Handle the current location
+        print("Current Location: ${value.latitude}, ${value.longitude}");
+      } else {
+        print("Failed to get current location");
+      }
+    }).catchError((error) {
+      print("Error getting current location: $error");
+    });
+  }
+
+  @override
+  void initState() {
+    getCurrentLocation();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: kPages[_currentPage],
+      body: IndexedStack(index: _currentPage, children: kPages),
       bottomNavigationBar: CustomNavBar(
         selectedIndex: _currentPage,
         onItemSelected: (val) {
